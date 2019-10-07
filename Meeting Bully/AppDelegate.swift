4803acec239c6b8bb0734636cc173c1cfd19978d
Mike Crittenden <mikecrittenden@gmail.com>
2019-10-07 13:41:05 -0400

    Initial Commit

---
+++ Meeting Bully/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Meeting Bully
+//
+//  Created by Mike Crittenden on 10/7/19.
+//  Copyright © 2019 Mike Crittenden. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Meeting Bully/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Meeting Bully
+//
+//  Created by Mike Crittenden on 10/7/19.
+//  Copyright © 2019 Mike Crittenden. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
