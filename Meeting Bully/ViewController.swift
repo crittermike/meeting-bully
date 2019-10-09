da47d711de6e0e4c4189ff5bbe0d70b33667fda6
Mike Crittenden <mikecrittenden@gmail.com>
2019-10-09 19:44:45 -0400

    Trying to get auto-adding of buttons to work

--- Meeting Bully/ViewController.swift
+++ Meeting Bully/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: NSViewController {
     @IBOutlet weak var titleLabelView: NSTextFieldCell!
     @IBOutlet weak var descriptionLabelView: NSTextFieldCell!
     
-    override func viewWillAppear() {
-        super.viewWillAppear()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         eventStore.requestAccess(to: .event) { granted, error in
             if granted {
                 self.getTodayEvents()
@@ -44,12 +45,27 @@ class ViewController: NSViewController {
 
                 for event in events {
                     if !event.isAllDay {
-                        self.descriptionLabelView.stringValue.append(event.notes ?? "")
-                        self.titleLabelView.stringValue.append(event.title)
+                        self.descriptionLabelView.stringValue = "";
+                        
+                        let input = (event.notes ?? "").replacingOccurrences(of: "<[^>]+>", with: " ", options: .regularExpression, range: nil)
+                        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+                        let matches = detector.matches(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count))
+
+                        for match in matches {
+                            guard let range = Range(match.range, in: input) else { continue }
+                            let url = input[range]
+                            let button = NSButton()
+                            button.title = "wtf"
+                            if (url.contains("bluejeans.com")) {
+                                button.title = "Join BlueJeans"
+                            }
+                            self.view.addSubview(button)
+                        }
+                        
+                        self.titleLabelView.stringValue = event.title
                     }
                 }
             }
         }
     }
 }
-
